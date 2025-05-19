a737bede7f112cf61356e08c72b40b9051b4828e
Titi7834 <tiago.dc78@gmail.com>
2025-05-19 14:00:52 +0200

    gitignore + pages et css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BookPage from './pages/BookPage';
 import DashboardPage from './pages/DashboardPage';
+import './App.css'; // Import CSS file for styling
 
 function App() {
  return (
