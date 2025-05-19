4a1d4863b6460a72731a339efd6f7359632438ba
Titi7834 <tiago.dc78@gmail.com>
2025-05-19 11:08:11 +0200

    initialisation react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MyLink } from './Mylink';
+
+const App = () => {
+  return <div>
+    Hello, world!
+    <br/>
+    <MyLink />
+  </div>;
+};
+export default App;
---
+++ src/Mylink.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MyLink = () => {
+    return (
+        <a href="https://coolors.co" target="_blank">
+            Click here to visit Coolors
+        </a>
+    );
+}
