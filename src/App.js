ecf467f57eb30940eb79b4aaac3552ee62d42962
Killian <killian.doizelet@supdevinci-edu.fr>
2025-05-22 09:34:58 +0200

    Update avec NavBar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BookPage from './pages/BookPage';
 import DashboardPage from './pages/DashboardPage';
-import './pages/HomePage.css'; // Import CSS file for HomePage
+import './pages/HomePage.css';
+import './pages/BookPage.css'; 
+import './pages/DashboardPage.css'; 
 
 function App() {
  return (
