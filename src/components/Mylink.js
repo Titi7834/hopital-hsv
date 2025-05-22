893f6aa91f2a6933fbfff745b97aef40d03a0da3
Titi7834 <tiago.dc78@gmail.com>
2025-05-22 14:24:43 +0200

    Suppression MyLink.js

--- src/components/Mylink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const MyLink = () => {
-    return (
-        <a href="https://coolors.co" target="_blank">
-            Click here to visit Coolors
-        </a>
-    );
-}
